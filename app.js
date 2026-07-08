fe5a63307308ad074239ce544fd430b4729e13ab
Thiago <thiagobelpo@gmail.com>
2026-07-08 20:36:38 -0300

    feat: actualizar respuesta del servidor para retornar JSON en la ruta raíz

--- app.js
+++ app.js
@@ -3,8 +3,24 @@ import http from "http";
 const puerto = 3000;
 
 const servidor = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello, World!\n");
+
+  // if(req.url === "/") {
+  //   res.statusCode = 200;
+  //   res.setHeader("Content-Type", "text/plain");
+
+  //   res.writeHead(200, { "Content-Type": "text/plain" });
+  //   res.end("Bienvenido a la API de Media!\n");
+  // }
+
+if(req.url === "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "Application/json");
+    res.end(JSON.stringify({ message: "Bienvenido a la API de Media!" }));
+  }
+  else{
+    res.statusCode = 404;
+  }
+
 });
 
 servidor.listen(puerto, () => {
