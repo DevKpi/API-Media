db9fa3d751318f4e8c447cc8bbe51d9a1e4b6a59
Thiago <thiagobelpo@gmail.com>
2026-07-08 20:43:45 -0300

    refactor: mejorar la estructura del servidor y restaurar rutas de respuesta

--- app.js
+++ app.js
@@ -4,21 +4,23 @@ const puerto = 3000;
 
 const servidor = http.createServer((req, res) => {
 
-  // if(req.url === "/") {
-  //   res.statusCode = 200;
-  //   res.setHeader("Content-Type", "text/plain");
-
-  //   res.writeHead(200, { "Content-Type": "text/plain" });
-  //   res.end("Bienvenido a la API de Media!\n");
-  // }
-
 if(req.url === "/") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "Application/json");
     res.end(JSON.stringify({ message: "Bienvenido a la API de Media!" }));
   }
+  else if(req.url === "/texto"){
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Bienvenido a la API de Media!\n");
+  }
+  else if(req.url === "/html"){
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>Bienvenido a la API de Media!</h1>");
+  }
   else{
     res.statusCode = 404;
+    res.setHeader("Content-Type", "Application/json");
+    res.end(JSON.stringify({ message: "Ruta no encontrada" }));
   }
 
 });
