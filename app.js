c89ff28240db41c0f0eb5234e19672d783fa6af8
Thiago <thiagobelpo@gmail.com>
2026-07-08 20:49:55 -0300

    feat: agregar archivo .gitignore y actualizar app.js para mejorar la gestión de rutas

--- app.js
+++ app.js
@@ -4,25 +4,22 @@ const puerto = 3000;
 
 const servidor = http.createServer((req, res) => {
 
-if(req.url === "/") {
+  if (req.url === "/") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "Application/json");
     res.end(JSON.stringify({ message: "Bienvenido a la API de Media!" }));
-  }
-  else if(req.url === "/texto"){
+  } else if (req.url === "/texto") {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Bienvenido a la API de Media!\n");
-  }
-  else if(req.url === "/html"){
+  } else if (req.url === "/html") {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end("<h1>Bienvenido a la API de Media!</h1>");
-  }
-  else{
+  } else {
     res.statusCode = 404;
     res.setHeader("Content-Type", "Application/json");
     res.end(JSON.stringify({ message: "Ruta no encontrada" }));
   }
-
+  
 });
 
 servidor.listen(puerto, () => {
