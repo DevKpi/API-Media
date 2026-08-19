5725f0e71e0b3cd07c96e866db21422a6058860b
Thiago <thiagobelpo@gmail.com>
2026-08-19 20:06:01 -0300

    feat: agregar ruta para obtener la lista de personas

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ const puerto = 3000;
 
 const app = express();
 
+const persona = [
+  { id: 1, nombre: "Leonardo", apellido: "Eckert" },
+  { id: 2, nombre: "Fabrizio", apellido: "Perez" },
+  { id: 3, nombre: "Esteban", apellido: "Balladares" }
+];
+
 app.get("/", (req, res) => {
   res.json({ message: "Bienvenido a la API de Media!" });
+});
+
+app.get("/persona", (req, res) => {
+  res.json(persona);
 });
 
 app.get("/texto", (req, res) => {
