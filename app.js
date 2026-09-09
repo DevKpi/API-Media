f6de059d52df854571aec242d89b8e60a5d7a6c5
Thiago <thiagobelpo@gmail.com>
2026-09-09 20:13:06 -0300

    feat: integrar verificación de conexión a BD y exportar app para pruebas

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from "express";
-
+import dotenv from "dotenv";
+import { probarConexion } from "./src/config/db.js";
 import usuarioRoutes from "./src/routes/UsuarioRoute.js";
 
-const puerto = 3000;
+dotenv.config();
+
+const puerto = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
@@ -13,7 +16,11 @@ app.get("/", (req, res) => {
 
 app.use("/usuarios", usuarioRoutes);
 
+if (process.env.NODE_ENV !== "test") {
+  app.listen(puerto, async () => {
+    console.log(`Servidor escuchando en http://localhost:${puerto}`);
+    await probarConexion();
+  });
+}
 
-app.listen(puerto, () => {
-  console.log(`Servidor escuchando en http://localhost:${puerto}`);
-});
+export default app;
